8069f132ff98b97ab35eb21ca0e61fc097344c41
Ecem <ecembostancioglu21@gmail.com>
2023-12-30 01:56:00 +0300

    TableView was created on ViewController

--- familyGuyBook/DetailsVC.swift
+++ familyGuyBook/DetailsVC.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class DetailsVC: UIViewController {
 
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var myNameLabel: UILabel!
+    
+    @IBOutlet weak var myJobLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- familyGuyBook/ViewController.swift
+++ familyGuyBook/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Family Guy"
+        return cell
+    }
+    
 
 }
 
