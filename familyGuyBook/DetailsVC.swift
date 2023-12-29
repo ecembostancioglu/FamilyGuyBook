a2a9829bca3a0a80ef4e5de3ea272f740255b08a
Ecem <ecembostancioglu21@gmail.com>
2023-12-30 00:34:10 +0300

    View Controller and DetailsVC were created and objects were added

---
+++ familyGuyBook/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  familyGuyBook
+//
+//  Created by Ecem Bostancıoğlu on 30.12.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
